77175706bc9e96fde00ff86a2c47bb713f39f104
mdriazuddin417 <mdriazuddin417@gmail.com>
2022-07-28 08:14:56 +0600

    input design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Login from "./components/authentication/Login";
+// import Login from "./components/authentication/Login";
+import SignUp from "./components/authentication/SignUp";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login />} />
+        {/* <Route path="/" element={<Login />} /> */}
+        <Route path="/" element={<SignUp />} />
       </Routes>
     </div>
   );
---
+++ src/components/authentication/SignUp.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import ScanCode from "./ScanCode";
+import authentication from "../../image/authenticaion1.png";
+import { ImArrowRight2 } from "react-icons/im";
+import "./authentication.css";
+const SignUp = () => {
+  const [name, setName] = useState("");
+  const [regNumber, setRegNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [otp, setOtp] = useState("");
+  return (
+    <div className="grid lg:grid-cols-5  grid-cols-1 h-screen ">
+      <div className="  flex flex-col col-span-2  justify-center items-center p-5">
+        <h2 className="text-black text-3xl font-bold py-7 ">Welcome back </h2>
+        <div className="w-full space-y-5 px-5">
+          <div className="w-full space-y-3 ">
+            <div className="form relative">
+              <input
+                type="text"
+                onChange={(e) => setName(e.target.value)}
+                className="textbox"
+                placeholder=" "
+              />
+              <label htmlFor="" className="form-label">
+                Company name
+              </label>
+            </div>
+            <div className="form relative">
+              <input
+                type="text"
+                onChange={(e) => setRegNumber(e.target.value)}
+                className="textbox"
+                placeholder=" "
+              />
+              <label htmlFor="" className="form-label">
+                Registration Number
+              </label>
+            </div>
+            <div className="form relative">
+              <input
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                className="textbox"
+                placeholder=" "
+              />
+              <label htmlFor="" className="form-label">
+                Company Email
+              </label>
+            </div>
+            <div className="form relative">
+              <input
+                type="text"
+                onChange={(e) => setOtp(e.target.value)}
+                className="textbox"
+                placeholder=" "
+              />
+              <label htmlFor="" className="form-label">
+                OTP
+              </label>
+            </div>
+          </div>
+
+          <button className="  w-full h-[70px] bg-primary text-[22px] font-bold rounded text-white">
+            Submit
+          </button>
+        </div>
+        <ScanCode />
+      </div>
+      <div className="bg-primary  flex flex-col  justify-center items-center text-white lg:px-10 col-span-3  gap-6 lg:p-10 p-3 text-center">
+        <div>
+          <h3 className="font-bold lg:text-[34px] text-xl mb-2">
+            Tip of the day :
+          </h3>
+          <p className="lg:text-[30px] text-xl">
+            Never share your wallet password with anyoune
+          </p>
+        </div>
+        <img src={authentication} alt="" className="w-2/3" />
+        <p className="lg:text-[24px] text-xl text-center">
+          The weakeset link in security is the human factor. Never share your
+          wallet passsword. Also, never login untrusted devices as the password
+          might be save in keychain.
+        </p>
+        <p className="lg:text-[26px] text-xl font-bold flex justify-center items-center gap-2 cursor-pointer">
+          <span> More on blog</span> <ImArrowRight2 />
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
